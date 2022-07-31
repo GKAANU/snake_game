daa2c1846df03bf0f33ecae06e0c58c6f9f75b9c
GKAANU <kaan.uzun.61@outlook.com>
2022-07-31 20:29:42 +0300

    Snake game ended

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from turtle import Screen
 from snake import Snake
+from food import Food
+from scoreboard import Scoreboard
 import time
+
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -9,6 +12,8 @@ screen.title("My Snake Game")
 screen.tracer(0)
 
 snake = Snake()
+food = Food()
+board = Scoreboard()
 
 screen.listen()
 screen.onkey(snake.up, "Up")
@@ -20,8 +25,21 @@ game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(0.1)
-
     snake.move()
+    score = 0
 
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        board.increase_score()
+        snake.extend()
+
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        game_is_on = False
+        board.game_over()
+
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
+            game_is_on = False
+            board.game_over()
 
 screen.exitonclick()
---
+++ scoreboard.py
@@ -0,0 +1,29 @@
+from turtle import Turtle
+ALINGMENT = "center"
+FONT = ("Courier", 24, "normal")
+
+
+class Scoreboard(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        self.hideturtle()
+        self.penup()
+        self.color("white")
+        self.goto(0, 265)
+        self.update_scoreboard()
+
+
+    def update_scoreboard(self):
+        self.clear()
+        self.write(f"Score: {self.score}", align=ALINGMENT, font=FONT)
+
+    def game_over(self):
+        self.goto(0, 0)
+        self.write("GAME OVER", align=ALINGMENT, font=FONT)
+
+    def increase_score(self):
+        self.score += 1
+        self.update_scoreboard()
+
